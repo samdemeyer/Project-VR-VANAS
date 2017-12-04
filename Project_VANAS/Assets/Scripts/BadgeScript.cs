d8af5566425a5cc5f4f2dbd0c8e8402399d94ef8
Unknown <michael.bellekens@student.kdg.be>
2017-12-04 14:55:42 +0100

    Adds badge scanner

---
+++ Project_VANAS/Assets/Scripts/BadgeScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BadgeScript : MonoBehaviour {
+
+    // Personal information
+    public string nameEmployee = "Michael Bellekens";
+    public string job = "Nurse";
+
+    // Vanaskast information
+    public Canvas welcomeScreen;
+    public Text nameText;
+    public Text jobText;
+    public Canvas screenUI;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "vanaskastScanner")
+        {
+            nameText.text = nameEmployee;
+            jobText.text = job;
+
+            welcomeScreen.gameObject.SetActive(true);
+            StartCoroutine(bootSystemVanaskast());
+        }
+    }
+
+    IEnumerator bootSystemVanaskast()
+    {
+        yield return new WaitForSeconds(3f);
+        welcomeScreen.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.2f);
+        screenUI.gameObject.SetActive(true);
+    }
+}
