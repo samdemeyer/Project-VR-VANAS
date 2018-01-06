9f7d97517dc2f95e4d2baf37427b1b8960ad1035
Sam <sam.demeyer@student.kdg.be>
2018-01-06 16:06:32 +0100

    Adds dissolve medicine in water

---
+++ Project_VANAS/Assets/Scripts/WaterScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScript : MonoBehaviour {
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.childCount >= 1) { 
+            if (collision.gameObject.transform.GetChild(0).tag == "DissolvableMedTop")
+            {
+            Debug.Log("Change color");
+            setColor(Color.red);
+            }
+        }
+    }
+
+    protected void setColor(Color _newcolor)
+    {
+        GameObject button = gameObject.transform.GetChild(0).gameObject;
+        Renderer buttonColor = button.GetComponent<Renderer>();
+        buttonColor.material.color = _newcolor;
+    }
+}
